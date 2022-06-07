a589eaeca21d1e5d104431d629c0dcd9d8ca2b9a
anton <azvontsov@gmail.com>
2022-06-07 17:39:00 -0400

    added gallery page

---
+++ src/components/Gallery/GalleryElements.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+  min-height: 692px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 0;
+  overflow: hidden;
+  background: linear-gradient(180deg, #424242 0%, #8d8e8e 100%);
+`;
+
+export const GalleryWrap = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (max-width: 400px) {
+    height: 80%;
+  }
+`;
+
+export const GalleryContent = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (max-width: 480px) {
+    padding: 10px;
+  }
+`;
+
+export const Icon = styled(Link)`
+  margin-left: 32px;
+  margin-top: 32px;
+  text-decoration: none;
+  color: #fff;
+  font-weight: 700;
+  font-size: 32px;
+
+  @media screen and (max-width: 480px) {
+    margin-left: 16px;
+    margin-top: 8px;
+  }
+`;
+
+export const GalleryImg = styled.img`
+  width: 100px;
+  height: 100px;
+`;
