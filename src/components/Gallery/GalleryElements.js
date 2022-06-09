6fdf1af82396a0ec245bd976e7b5fd6b541b0fa3
anton <azvontsov@gmail.com>
2022-06-09 08:15:29 -0400

    adds favicon

--- src/components/Gallery/GalleryElements.js
+++ src/components/Gallery/GalleryElements.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Link as LinkScroll } from "react-scroll";
 
 export const Container = styled.div`
   height: 100vh;
